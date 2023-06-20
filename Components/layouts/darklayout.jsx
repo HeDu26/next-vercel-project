7fc7d7bc498d8d0dfbf06bfd0590f10ed7ffea68
Héctor Álvarez <hec.itp@hotmail.com>
2023-06-19 19:38:27 -0600

    First deplayment

---
+++ Components/layouts/darklayout.jsx
@@ -0,0 +1,9 @@
+export conts DarkLayout = ({children}) => {
+  return (
+    <div >
+      <h3>Dark  Layput</h3>
+      <div>
+      {children}
+      </div>
+    </div>
+}
---
+++ pages/dynamics/users/[id].tsx
@@ -0,0 +1,7 @@
+export default function Users({ id }) {
+  return (
+    <>
+      <h1>Dynamic Users</h1>
+    </>
+  );
+}
